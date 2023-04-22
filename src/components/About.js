750369630833a461b413fbd9d9453c89d9293ef2
Lionel <wafulalionel20@gmail.com>
2023-04-23 01:13:34 +0300

    Complete Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,11 @@ import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  return( <div id="about">
+   <h2>About Me</h2>
+   <p>My name is Lionel the programmer</p>
+   <img src={image} alt="I made this"></img>
+  </div>);
 }
 
 export default About;
